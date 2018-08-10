d7693f3ab24a7abe995a3646edbbc4242af44b05
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-10 22:00:23 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a,b|
+    if a == b
+      0
+    elsif a < b
+      1
+    else
+      -1
+    end      
+  end
+end
