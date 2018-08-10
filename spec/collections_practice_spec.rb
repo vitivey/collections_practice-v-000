f451f9141988265aa9370883f0d35696a3ac8ce6
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-10 22:07:24 +0000

    automatically backed up by learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -30,6 +30,13 @@ describe 'collections practice' do
     end
   end
 
+  # Question 4
+  describe '#swap_elements_from_to' do
+    it 'swap the second and third elements of an array using index parameters' do
+      expect(swap_elements_from_to(["blake", "ashley", "scott"])).to eq(["blake", "scott", "ashley"])
+    end
+  end
+
   # Question 5
   describe '#reverse_array' do
     it 'reverse the order of an array of integers' do
